d6030bef567567c255ac4948b9f790161ae9e137
somu <108333014+somuportfolio@users.noreply.github.com>
2022-12-15 21:44:36 +0530

    Tasks

---
+++ learn-node/exp.js
@@ -0,0 +1,28 @@
+//Server creation
+const http = require("http");
+/*
+const server = http.createServer((req,res) => {
+    res.write('<h1>hello</h1>');;
+    res.writeHead(200,{"content-type": "text/html"})
+    res.end(
+        JSON.stringify({
+            data:"hello man",
+        })
+    );
+});
+
+server.listen(3005) 
+
+/*
+const {rectangle} = require("./expo")
+
+console.log(rectangle(6))*/
+
+/*
+const rectangle = require("./expo")
+
+const rect =new rectangle();
+
+console.log(rect.area(4));
+console.log(rect.circum(6));
+*/
---
+++ learn-node/expo.js
@@ -0,0 +1,10 @@
+class rectangle{
+    area(r){
+        return Math.PI*r
+    }
+    circum(s){
+        return Math.exp*s
+    }
+}
+
+module.exports = rectangle;
